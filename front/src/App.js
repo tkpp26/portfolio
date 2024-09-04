a97b8e0d8ab1ff7479a501eb81b46411fccca1d7
Shizuka <tkpp26@gmail.com>
2024-09-03 18:26:39 -0700

    Added heading

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Helmet } from "react-helmet";
 import Home from "./components/Home";
+
 function App() {
-  console.log(process.env.REACT_APP_API_KEY);
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+    <Router>
+      <Helmet>
+        <title>Shizuka Takao - Personal Website</title>
+        <meta
+          name="description"
+          content="Explore Shizuka Takao's personal website. Shizuka is an aspiring software engineer currently learning machine learning and backend development."
+        />
+      </Helmet>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
